e0c75addfa5863d44a0b73426017d71e6782a89f
Olivier Yiptong <olivier@olivieryiptong.com>
2014-03-06 14:59:06 -0500

    identity assertion in head_newtab.js

--- toolkit/modules/tests/xpcshell/newtab/head_newtab.js
+++ toolkit/modules/tests/xpcshell/newtab/head_newtab.js
@@ -2,3 +2,21 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cu = Components.utils;
 const Cr = Components.results;
+
+function isIdentical(expected, actual) {
+  if (expected == null) {
+    do_check_eq(expected, actual);
+  }
+  else if (typeof expected == "object") {
+    // Make sure all the keys match up
+    do_check_eq(Object.keys(expected).sort() + "", Object.keys(actual).sort());
+
+    // Recursively check each value individually
+    Object.keys(expected).forEach(key => {
+      isIdentical(actual[key], expected[key]);
+    });
+  }
+  else {
+    do_check_eq(expected, actual);
+  }
+}
