6d8235921b8248aa9acf3abf610f368e5738bed7
Edward Lee <edilee@mozilla.com>
2014-03-07 00:12:37 -0800

    Remove trailing spaces.

--- toolkit/modules/tests/xpcshell/newtab/test_DirectoryTilesProvider.js
+++ toolkit/modules/tests/xpcshell/newtab/test_DirectoryTilesProvider.js
@@ -30,7 +30,7 @@ function run_test() {
 }
 
 add_task(function test_DirectoryTiles__tilesUrl() {
-  let provider = NewTabUtils._providers['directoryTiles'];  
+  let provider = NewTabUtils._providers['directoryTiles'];
   let exampleUrl = 'http://example.com';
   // tilesUrl is obtained from prefs
   Services.prefs.setCharPref(provider._prefs['tilesUrl'], exampleUrl);
@@ -43,7 +43,7 @@ add_task(function test_DirectoryTiles__tilesUrl() {
 add_task(function test_DirectoryTiles__tilesUrl_data_copy() {
   let dataURI = 'data:application/json,{"en-US":[{"url":"http://example.com","title":"example"}]}';
 
-  let provider = NewTabUtils._providers['directoryTiles'];  
+  let provider = NewTabUtils._providers['directoryTiles'];
   Services.prefs.setCharPref(provider._prefs['tilesUrl'], dataURI);
   do_check_eq(provider._tilesUrl, dataURI);
 
@@ -72,7 +72,7 @@ add_task(function test_DirectoryTiles__tilesUrl_locale() {
   };
   let dataURI = 'data:application/json,' + JSON.stringify(data);
 
-  let provider = NewTabUtils._providers['directoryTiles'];  
+  let provider = NewTabUtils._providers['directoryTiles'];
   Services.prefs.setCharPref(provider._prefs['tilesUrl'], dataURI);
   Services.prefs.setCharPref('general.useragent.locale', 'en-US');
 
@@ -95,7 +95,7 @@ add_task(function test_DirectoryTiles__tilesUrl_locale() {
 });
 
 add_task(function test_DirectoryTiles__prefObserver_url() {
-  let provider = NewTabUtils._providers['directoryTiles'];  
+  let provider = NewTabUtils._providers['directoryTiles'];
   Services.prefs.setCharPref(provider._prefs['tilesUrl'], kDefaultTileSource);
 
   // set up the observer
@@ -117,9 +117,9 @@ add_task(function test_DirectoryTiles__prefObserver_url() {
 });
 
 add_task(function test_DirectoryTiles_getLinks() {
-  let provider = NewTabUtils._providers['directoryTiles'];  
+  let provider = NewTabUtils._providers['directoryTiles'];
   Services.prefs.setCharPref(provider._prefs['tilesUrl'], kDefaultTileSource);
-  
+
   let links = yield fetchData(provider);
   do_check_true(links.length > 0);
   provider.reset();
